c3a175e6b010b43b2a3451ae768051eaa187eeff
John Hsu <hsu@osrfoundation.org>
2012-11-24 12:24:53 -0800

    fix for fire hose model

--- plugins/DRCFirehosePlugin.cc
+++ plugins/DRCFirehosePlugin.cc
@@ -86,7 +86,7 @@ void DRCFirehosePlugin::Load(physics::ModelPtr _parent,
   this->lastTime = this->world->GetSimTime();
 
   // Set initial configuration
-  // this->SetInitialConfiguration();
+  this->SetInitialConfiguration();
 
   // New Mechanism for Updating every World Cycle
   // Listen to the update event. This event is broadcast every
@@ -105,8 +105,9 @@ void DRCFirehosePlugin::SetInitialConfiguration()
   // for (unsigned int i = 0; i < this->links.size(); ++i)
   //   gzerr << "link [" << this->links[i]->GetName() << "]\n";
 
-  this->joints[17]->SetAngle(0, -M_PI/4.0);
-  this->joints[19]->SetAngle(0, -M_PI/4.0);
+  // should do this via setting initial state in sdf when it's working
+  this->joints[this->joints.size()-4]->SetAngle(0, -M_PI/4.0);
+  this->joints[this->joints.size()-2]->SetAngle(0, -M_PI/4.0);
 }
 
 
