9bcd6eda787010b5236fe7dce00e989a6cb748be
John Hsu <hsu@osrfoundation.org>
2012-12-19 14:44:17 -0800

    style fix

--- plugins/ros/DRCRobotPlugin.cc
+++ plugins/ros/DRCRobotPlugin.cc
@@ -231,8 +231,8 @@ void DRCRobotPlugin::OnLContactUpdate()
     }
     // low pass filter over time
     double e = 0.99;
-    this->lFootForce = this->lFootForce * e + fTotal*(1.0 - e);
-    this->lFootTorque = this->lFootTorque * e + tTotal*(1.0 - e);
+    this->lFootForce = this->lFootForce * e + fTotal * (1.0 - e);
+    this->lFootTorque = this->lFootTorque * e + tTotal * (1.0 - e);
 
     geometry_msgs::Wrench msg;
     msg.force.x = this->lFootForce.x;
@@ -289,8 +289,8 @@ void DRCRobotPlugin::OnRContactUpdate()
     }
     // low pass filter over time
     double e = 0.99;
-    this->rFootForce = this->rFootForce * e + fTotal*(1.0 - e);
-    this->rFootTorque = this->rFootTorque * e + tTotal*(1.0 - e);
+    this->rFootForce = this->rFootForce * e + fTotal * (1.0 - e);
+    this->rFootTorque = this->rFootTorque * e + tTotal * (1.0 - e);
 
     geometry_msgs::Wrench msg;
     msg.force.x = this->rFootForce.x;
