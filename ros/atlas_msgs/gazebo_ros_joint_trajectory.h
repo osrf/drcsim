b40ec40244e8c47bf0e805d2b35eb37234e7e1a7
John Hsu <hsu@osrfoundation.org>
2013-01-23 18:40:25 -0800

    style fixes

--- ros/atlas_msgs/gazebo_ros_joint_trajectory.h
+++ ros/atlas_msgs/gazebo_ros_joint_trajectory.h
@@ -18,6 +18,9 @@
 #ifndef GAZEBO_ROS_JOINT_TRAJECTORY_PLUGIN_HH
 #define GAZEBO_ROS_JOINT_TRAJECTORY_PLUGIN_HH
 
+#include <string>
+#include <vector>
+
 #include <boost/thread.hpp>
 #include <boost/thread/mutex.hpp>
 
@@ -25,6 +28,7 @@
 #include <ros/callback_queue.h>
 #include <ros/advertise_options.h>
 #include <ros/subscribe_options.h>
+
 #include <trajectory_msgs/JointTrajectory.h>
 #include <geometry_msgs/Pose.h>
 
@@ -41,86 +45,84 @@
 
 namespace gazebo
 {
-   class GazeboRosJointTrajectory : public ModelPlugin
-   {
-      /// \brief Constructor
-      public: GazeboRosJointTrajectory();
+  class GazeboRosJointTrajectory : public ModelPlugin
+  {
+    /// \brief Constructor
+    public: GazeboRosJointTrajectory();
 
-      /// \brief Destructor
-      public: virtual ~GazeboRosJointTrajectory();
+    /// \brief Destructor
+    public: virtual ~GazeboRosJointTrajectory();
 
-      /// \brief Load the controller
-      public: void Load( physics::ModelPtr _model, sdf::ElementPtr _sdf );
+    /// \brief Load the controller
+    public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf);
 
-      /// \brief Update the controller
-      private: void SetTrajectory(
-        const trajectory_msgs::JointTrajectory::ConstPtr& trajectory);
+    /// \brief Update the controller
+    private: void SetTrajectory(
+      const trajectory_msgs::JointTrajectory::ConstPtr& trajectory);
 #ifdef ENABLE_SERVICE
-      private: bool SetTrajectory(
-        const gazebo_msgs::SetJointTrajectory::Request& req,
-        const gazebo_msgs::SetJointTrajectory::Response& res);
+    private: bool SetTrajectory(
+      const gazebo_msgs::SetJointTrajectory::Request& req,
+      const gazebo_msgs::SetJointTrajectory::Response& res);
 #endif
-      private: void UpdateStates();
+    private: void UpdateStates();
 
-      private: physics::WorldPtr world_;
-      private: physics::ModelPtr model_;
+    private: physics::WorldPtr world_;
+    private: physics::ModelPtr model_;
 
-      /// \brief pose should be set relative to this link (default to "world")
-      private: physics::LinkPtr reference_link_;
-      private: std::string reference_link_name_;
-      /// \brief frame transform name, should match link name
-      //private: std::string tf_frame_name_;
+    /// \brief pose should be set relative to this link (default to "world")
+    private: physics::LinkPtr reference_link_;
+    private: std::string reference_link_name_;
 
-      /// \brief pointer to ros node
-      private: ros::NodeHandle* rosnode_;
-      private: ros::Subscriber sub_;
-      private: ros::ServiceServer srv_;
-      private: bool has_trajectory_;
+    /// \brief pointer to ros node
+    private: ros::NodeHandle* rosnode_;
+    private: ros::Subscriber sub_;
+    private: ros::ServiceServer srv_;
+    private: bool has_trajectory_;
 
-      /// \brief ros message
-      private: trajectory_msgs::JointTrajectory trajectory_msg_;
-      private: bool set_model_pose_;
-      private: geometry_msgs::Pose model_pose_;
+    /// \brief ros message
+    private: trajectory_msgs::JointTrajectory trajectory_msg_;
+    private: bool set_model_pose_;
+    private: geometry_msgs::Pose model_pose_;
 
-      /// \brief topic name
-      private: std::string topic_name_;
-      private: std::string service_name_;
+    /// \brief topic name
+    private: std::string topic_name_;
+    private: std::string service_name_;
 
-      /// \brief A mutex to lock access to fields that are
-      /// used in message callbacks
-      private: boost::mutex update_mutex;
+    /// \brief A mutex to lock access to fields that are
+    /// used in message callbacks
+    private: boost::mutex update_mutex;
 
-      /// \brief save last_time
-      private: common::Time last_time_;
+    /// \brief save last_time
+    private: common::Time last_time_;
 
-      // trajectory time control
-      private: common::Time trajectory_start;
-      private: unsigned int trajectory_index;
+    // trajectory time control
+    private: common::Time trajectory_start;
+    private: unsigned int trajectory_index;
 
-      // rate control
-      private: double update_rate_;
-      private: bool disable_physics_updates_;
-      private: bool physics_engine_enabled_;
+    // rate control
+    private: double update_rate_;
+    private: bool disable_physics_updates_;
+    private: bool physics_engine_enabled_;
 
-      /// \brief for setting ROS name space
-      private: std::string robot_namespace_;
+    /// \brief for setting ROS name space
+    private: std::string robot_namespace_;
 
-      private: ros::CallbackQueue queue_;
-      private: void QueueThread();
-      private: boost::thread callback_queue_thread_;
+    private: ros::CallbackQueue queue_;
+    private: void QueueThread();
+    private: boost::thread callback_queue_thread_;
 
-      private: std::vector<gazebo::physics::JointPtr> joints_;
-      private: std::vector<trajectory_msgs::JointTrajectoryPoint> points_;
+    private: std::vector<gazebo::physics::JointPtr> joints_;
+    private: std::vector<trajectory_msgs::JointTrajectoryPoint> points_;
 
-      // Pointer to the update event connection
-      private: event::ConnectionPtr update_connection_;
+    // Pointer to the update event connection
+    private: event::ConnectionPtr update_connection_;
 
-      private: trajectory_msgs::JointTrajectory joint_trajectory_;
+    private: trajectory_msgs::JointTrajectory joint_trajectory_;
 
-      // deferred load in case ros is blocking
-      private: sdf::ElementPtr sdf;
-      private: void LoadThread();
-      private: boost::thread deferred_load_thread_;
-   };
+    // deferred load in case ros is blocking
+    private: sdf::ElementPtr sdf;
+    private: void LoadThread();
+    private: boost::thread deferred_load_thread_;
+  };
 }
 #endif
