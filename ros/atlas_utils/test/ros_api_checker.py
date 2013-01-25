bae35a66c02dbe9a9919c7d88b6a4ac610ce80a1
Brian Gerkey <gerkey@osrfoundation.org>
2013-01-25 11:36:17 -0800

    working on api_checker

--- ros/atlas_utils/test/ros_api_checker.py
+++ ros/atlas_utils/test/ros_api_checker.py
@@ -5,46 +5,41 @@ import unittest
 import subprocess
 import sys
 
-try:
-    import yaml
-except:
-    print('WARNING: Failed to import yaml, so skipping ROS API checks.  On Ubuntu: sudo apt-get install python-yaml')
-    # TODO: synthesize skipped test result file
-    sys.exit(1)
+class Tester(unittest.TestCase):
 
-def go(argv):
-    if len(argv) < 2:
-        print('Not enough args')
-        #TODO: fail
-        sys.exit(1)
-    topics = load(argv[1:])
-
-def test_topic(t):
-    cmd = ['rostopic', 'info', t.topic]
-    po = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    out, err = po.communicate()
-    if po.returncode:
-    
-
-def load(files):
-    topics = []
-    files = argv[1:]
-    for f in files:
+    def setUp(self):
+        self.argv = sys.argv
+        self.skip = False
         try:
-            y = yaml.load(open(f))        
-            ts = y['topics']
-            for t in ts:
-                # reference all fields to cause exception
-                s = t['topic']
-                s = t['type']
-                s = t['num_publishers']
-                s = t['num_subscribers']
-            topics.extend(ts)
-        except Exception as e:
-            print('Parsing %s failed with: %s'%(f,`e`))
-            #TODO: fail
-            sys.exit(1)
-    return topics
+            import yaml
+        except:
+            print('WARNING: Failed to import yaml, so skipping ROS API checks.  On Ubuntu: sudo apt-get install python-yaml')
+            self.skip = True
+
+    def test_api(self):
+        self.assertTrue(len(self.argv) > 2, msg='Not enough args')
+        topics = self._load(self.argv[1:])
+    
+    def _test_topic(t):
+        cmd = ['rostopic', 'info', t.topic]
+        po = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = po.communicate()
+        #if po.returncode:
+        
+    
+    def _load(files):
+        topics = []
+        files = argv[1:]
+        for f in files:
+            # Let parsing exceptions leak out; they'll be marked as test errors
+            y = yaml.load(open(f))
+            self.assertIn('topics', y)
+            self.assertIn('topic', y['topics'])
+            self.assertIn('topic', y['type'])
+            self.assertIn('topic', y['num_publishers'])
+            self.assertIn('topic', y['num_subscribers'])
+            topics.extend(y['topics'])
+        return topics
 
 if __name__ == '__main__':
-    go(sys.argv)
+    unittest.main()
