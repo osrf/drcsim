ea8a583f9ed765b5d77a093dd9a432cb1216a43f
Steve Peters <scpeters@osrfoundation.org>
2013-02-01 11:29:57 -0800

    Include tcpNoDelay in jointStates subscriber example as well

--- ros/atlas_msgs/pub_joint_commands.cpp
+++ ros/atlas_msgs/pub_joint_commands.cpp
@@ -142,7 +142,8 @@ int main(int argc, char** argv)
   // Note that we'll still accept TCP connections for this topic
   // (e.g., from rospy nodes, which don't support UDP);
   // we just prefer UDP.
-  jointStatesSo.transport_hints = ros::TransportHints().unreliable().reliable();
+  jointStatesSo.transport_hints =
+    ros::TransportHints().unreliable().reliable().tcpNoDelay(true);
 
   ros::Subscriber subJointStates = rosnode->subscribe(jointStatesSo);
   // ros::Subscriber subJointStates =
