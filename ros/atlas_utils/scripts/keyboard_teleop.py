10cb27ecc4d70bbc9f6798b3a1057277e3e8923c
brawner <devnull@localhost>
2013-04-09 19:14:40 -0700

    Minor tweaks

--- ros/atlas_utils/scripts/keyboard_teleop.py
+++ ros/atlas_utils/scripts/keyboard_teleop.py
@@ -24,21 +24,21 @@ class AtlasTeleop():
                   'I': {"forward":1, "lateral":0, "turn": 0}, \
                   'o': {"forward":1, "lateral":0, "turn": -1}, \
                   'O': {"forward":1, "lateral":0, "turn": -1}, \
-                  'j': {"forward":0, "lateral":0.5, "turn": 0}, \
-                  'J': {"forward":0, "lateral":0.5, "turn": 0}, \
+                  'j': {"forward":0, "lateral":0.25, "turn": 0}, \
+                  'J': {"forward":0, "lateral":0.25, "turn": 0}, \
                   'k': {"forward":0, "lateral":0, "turn": 0}, \
                   'K': {"forward":-1, "lateral":0, "turn": 0}, \
-                  'l': {"forward":0, "lateral":-0.5, "turn": 0}, \
-                  'L': {"forward":0, "lateral":-0.5, "turn": 0}, \
-                  'm': {"forward":0, "lateral":0, "turn": 1}, \
+                  'l': {"forward":0, "lateral":-0.25, "turn": 0}, \
+                  'L': {"forward":0, "lateral":-0.25, "turn": 0}, \
+                  'm': {"forward":0, "lateral":0, "turn": 0.5}, \
                   ',': {"forward":-0.5, "lateral":0, "turn": 0}, \
-                  '.': {"forward":0, "lateral":0, "turn": -1}}
+                  '.': {"forward":0, "lateral":0, "turn": -0.5}}
     
     params = {"stride_length":{ "value":0.25, "min":0, "max":1, "type":"float"},
               "stride_duration":{ "value":0.63, "min": 0, "max":100, "type":"float"},
-              "sequence_length":{"value":4, "min":2, "max":100, "type":"int"},
+              "sequence_length":{"value":5, "min":2, "max":100, "type":"int"},
               "stride_width":{"value":0.2, "min":0, "max":1, "type":"float"},
-              "turn_size":{"value":math.pi/16, "min":0.01, "max":math.pi/2, "type":"float"},
+              "in_place_turn_size":{"value":math.pi/32, "min":0.01, "max":math.pi/2, "type":"float"},
               "turn_radius":{"value":1, "min":0.01, "max":100, "type":"float"},
               "swing_height":{"value":0.3, "min":0, "max":1, "type":"float"}}
     
@@ -81,12 +81,12 @@ class AtlasTeleop():
            j    k    l
            m    ,    .
         
-        
+        2-9: Change the length of step trajectory
         E: View and Edit Parameters
         R: Reset robot to standing pose
         Q: Quit
         """
-        print(msg)      
+        self.debug(msg)      
         
     def resetToStanding(self):
         self.mode.publish("harnessed")
@@ -101,46 +101,42 @@ class AtlasTeleop():
         rospy.sleep(1)
     
     def twist(self, forward, lateral, turn):
-        print("Walking + " + str(self.params["sequence_length"]["value"]) + " steps")
+        self.debug("Walking " + str(self.params["sequence_length"]["value"]) + " steps")
         steps = []
         
         L = self.params["stride_length"]["value"]
-        y_offset = self.params["stride_width"]["value"]/2
+        R = self.params["turn_radius"]["value"]
+        W = self.params["stride_width"]["value"]
         X = 0
         Y = 0
         theta = 0
         dTheta = 0
         
         if forward != 0:
-            dTheta = turn * 2 * math.asin(L/(2*self.params["turn_radius"]["value"]))
+            dTheta = turn * 2 * math.asin(L/(2*R))
         else:
-            dTheta = turn * self.params["turn_size"]["value"]
+            dTheta = turn * self.params["in_place_turn_size"]["value"]
         steps = []
-        
         for i in range(self.params["sequence_length"]["value"]):
-            R = self.params["turn_radius"]["value"]
-            theta += dTheta
+            theta += (turn!=0) * (i%2) * dTheta
+            #left = 1, right = -1
             foot = 1 - 2*(i%2)
-
-
-            dX = forward - foot * turn * math.sin(dTheta)
-            dY = lateral - foot * turn * math.cos(dTheta)
             
-            X += dX * self.params["stride_length"]["value"]
-            Y += dY * self.params["stride_length"]["value"]
-            print("forward " + str(forward) + " lateral " + str(lateral) + " turn " + str(turn))
-            print("X " + str(X) + " Y " + str(Y) + " theta " + str(theta))
-            #X += dX / math.sqrt(dX*dX + dY*dY) + math.sin(theta) * (i%2)*0.2
-            #Y += dY / math.sqrt(dX*dX + dY*dY) + math.cos(theta) * (i%2)*0.2
+            if turn == 0:
+                X = (forward!=0) * (X + forward * L)
+                Y = (lateral!=0) * (Y + lateral * L) - foot * W/2
+            else:
+                X = forward * turn * R * math.sin(theta) - foot * W/2 * math.sin(theta)
+                Y = forward * turn * (R - R * math.cos(theta)) - foot * W/2 * math.cos(theta)
+            
             Q = quaternion_from_euler(0,0,theta)
-
 
             step = AtlasBehaviorStepParams()
             step.step_index = i+1
             step.foot_index = i % 2
             step.duration = self.params["stride_duration"]["value"]
             step.pose.position.x = X
-            step.pose.position.y = Y + foot * self.params["stride_width"]["value"]/2
+            step.pose.position.y = Y
             step.pose.position.z = 0.1
             step.pose.orientation.x = Q[0]
             step.pose.orientation.y = Q[1]
@@ -148,15 +144,19 @@ class AtlasTeleop():
             step.pose.orientation.w = Q[3]
             step.swing_height = self.params["swing_height"]["value"]
             steps.append(step)
-            
+        
+        #Add final step to bring feet together
+        #left = 1, right = -1
         foot = 1 - 2 * (1 - steps[-1].foot_index)
+        X = X - foot * W/2 * math.sin(theta)
+        Y = Y - foot * W/2 * math.cos(theta)
         Q = quaternion_from_euler(0,0,theta)
         step = AtlasBehaviorStepParams()
         step.step_index = len(steps) + 1
         step.foot_index = 1 - steps[-1].foot_index
         step.duration = self.params["stride_duration"]["value"]
         step.pose.position.x = X
-        step.pose.position.y = Y + foot * self.params["stride_width"]["value"]/2
+        step.pose.position.y = Y
         step.pose.position.z = 0.1
         step.pose.orientation.x = Q[0]
         step.pose.orientation.y = Q[1]
@@ -189,18 +189,19 @@ class AtlasTeleop():
         for i in range(len(self.params)):
             param_name = self.params.keys()[i]
             print(str(i) + " : " + param_name.ljust(maxLength+1) + str(self.params[param_name]["value"]))
-        print("E : Exit")
+        print("X : Exit")
         hasNumber = False
         selection = -1
-        while selection < 0 or selection > len(self.params):
+        while selection < 0 or selection >= len(self.params):
             var = raw_input("Enter number of param you want to change: ")
+            if var == 'x' or var == 'X':
+                self.printUsage()
+                return
             try:
                 selection = int(var)
             except ValueError:
                 selection = -1
-        if selection == len(self.params):
-            self.printUsage()
-            return
+        
         param = self.params.keys()[selection]
         value = 0
         valid = False
@@ -218,6 +219,10 @@ class AtlasTeleop():
         self.params[param]["value"] = value
         self.editParams()
         
+    def debug(self, str):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        print(str)
+        tty.setraw(sys.stdin.fileno())
     
     def processKey(self, ch):
         if self.directions.has_key(ch):
@@ -230,6 +235,12 @@ class AtlasTeleop():
             self.printUsage()
         elif ch == 'q' or ch == 'Q':
             rospy.signal_shutdown("Shutdown")
+        try:
+            if (int(ch) > 1):
+                self.params["sequence_length"]["value"] = int(ch)
+                self.debug("sequence_length: " + str(self.params["sequence_length"]["value"]))
+        except ValueError:
+            pass
             
     def getKey(self):
         tty.setraw(sys.stdin.fileno())
