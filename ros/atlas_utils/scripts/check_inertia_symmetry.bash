998c20e9a91596c11492a1310a8b9c08d171ab36
Steve Peters <scpeters@osrfoundation.org>
2013-04-11 16:07:48 -0700

    Move inertia checking script to atlas_utils, change arguments so it can be invoked with 'rosrun atlas_utils check_inertia_symmetry.bash ___' where ___ can be atlas or atlas_sandia_hands or atlas_irobot_hands

--- ros/atlas_utils/scripts/check_inertia_symmetry.bash
+++ ros/atlas_utils/scripts/check_inertia_symmetry.bash
@@ -1,24 +1,25 @@
 #!/bin/bash
+urdfFile=`rospack find $1`/$1.urdf
 echo link,mass,ixx,iyy,izz,ixy,ixz,iyz
 for suffix in \
- `xml sel -t -v '//link/@name' $1 | \
+ `xmlstarlet sel -t -v '//link/@name' $urdfFile | \
   grep '^r_' | sed -e 's@^r@@'`
 do
-  xml sel -t -o "r$suffix," \
+  xmlstarlet sel -t -o "r$suffix," \
           -t -v "//link[@name='r$suffix']/inertial/mass/@value"  -t -o ',' \
           -t -v "//link[@name='r$suffix']/inertial/inertia/@ixx" -t -o ',' \
           -t -v "//link[@name='r$suffix']/inertial/inertia/@iyy" -t -o ',' \
           -t -v "//link[@name='r$suffix']/inertial/inertia/@izz" -t -o ',' \
           -t -v "//link[@name='r$suffix']/inertial/inertia/@ixy" -t -o ',' \
           -t -v "//link[@name='r$suffix']/inertial/inertia/@ixz" -t -o ',' \
-          -t -v "//link[@name='r$suffix']/inertial/inertia/@iyz" $1
+          -t -v "//link[@name='r$suffix']/inertial/inertia/@iyz" $urdfFile
 
-  xml sel -t -o "l$suffix," \
+  xmlstarlet sel -t -o "l$suffix," \
           -t -v "//link[@name='l$suffix']/inertial/mass/@value"  -t -o ',' \
           -t -v "//link[@name='l$suffix']/inertial/inertia/@ixx" -t -o ',' \
           -t -v "//link[@name='l$suffix']/inertial/inertia/@iyy" -t -o ',' \
           -t -v "//link[@name='l$suffix']/inertial/inertia/@izz" -t -o ',' \
           -t -v "//link[@name='l$suffix']/inertial/inertia/@ixy" -t -o ',' \
           -t -v "//link[@name='l$suffix']/inertial/inertia/@ixz" -t -o ',' \
-          -t -v "//link[@name='l$suffix']/inertial/inertia/@iyz" $1
+          -t -v "//link[@name='l$suffix']/inertial/inertia/@iyz" $urdfFile
 done
