168d3572859c904f09ded247b7a3bed1199edc25
brawner <devnull@localhost>
2013-04-15 15:00:25 -0700

    More stable lateral walking

--- ros/atlas_utils/scripts/keyboard_teleop.py
+++ ros/atlas_utils/scripts/keyboard_teleop.py
@@ -30,28 +30,31 @@ class AtlasTeleop():
                   'I': {"forward":1, "lateral":0, "turn": 0}, \
                   'o': {"forward":1, "lateral":0, "turn":-1}, \
                   'O': {"forward":1, "lateral":0, "turn":-1}, \
-                  'j': {"forward":0, "lateral":0.25, "turn": 0}, \
-                  'J': {"forward":0, "lateral":0.25, "turn": 0}, \
+                  'j': {"forward":0, "lateral":1, "turn": 0}, \
+                  'J': {"forward":0, "lateral":1, "turn": 0}, \
                   'k': {"forward":0, "lateral":0, "turn": 0}, \
                   'K': {"forward":-1, "lateral":0, "turn": 0}, \
-                  'l': {"forward":0, "lateral":-0.25, "turn": 0}, \
-                  'L': {"forward":0, "lateral":-0.25, "turn": 0}, \
+                  'l': {"forward":0, "lateral":-1, "turn": 0}, \
+                  'L': {"forward":0, "lateral":-1, "turn": 0}, \
                   'm': {"forward":0, "lateral":0, "turn": 0.5}, \
                   ',': {"forward":-0.5, "lateral":0, "turn": 0}, \
                   '.': {"forward":0, "lateral":0, "turn":-0.5}}
     
     # BDI Controller bindings
-    params = {"stride_length":{ "value":0.15, "min":0, "max":1, \
+    params = {"Forward Stride Length":{ "value":0.15, "min":0, "max":1, \
                                 "type":"float"},
-              "step_height":{"value":0, "min":-1, "max":1, "type":"float"},
-              "stride_duration":{ "value":0.63, "min": 0, "max":100, \
-                                  "type":"float"},
-              "sequence_length":{"value":5, "min":1, "max":100, "type":"int"},
-              "stride_width":{"value":0.2, "min":0, "max":1, "type":"float"},
-              "in_place_turn_size":{"value":math.pi / 16, "min":0, \
+              "Lateral Stride Length":{ "value":0.15, "min":0, "max":1, \
+                                "type":"float"},
+              "Step Height":{"value":0, "min":-1, "max":1, "type":"float"},
+              "Stride Duration":{ "value":0.63, "min": 0, "max":100, \
+                                "type":"float"},
+              "Walk Sequence Length":{"value":5, "min":1, "max":sys.maxint, \
+                                "type":"int"},
+              "Stride Width":{"value":0.2, "min":0, "max":1, "type":"float"},
+              "In Place Turn Size":{"value":math.pi / 16, "min":0, \
                                     "max":math.pi / 2, "type":"float"},
-              "turn_radius":{"value":2, "min":0.01, "max":100, "type":"float"},
-              "swing_height":{"value":0.3, "min":0, "max":1, "type":"float"}}
+              "Turn Radius":{"value":2, "min":0.01, "max":100, "type":"float"},
+              "Swing Height":{"value":0.3, "min":0, "max":1, "type":"float"}}
     
     def init(self):
         # Saves terminal settings
@@ -124,12 +127,13 @@ class AtlasTeleop():
     # Param turn: 1 Counter clockwise turn, -1 clockwise turn    
     def twist(self, forward, lateral, turn):
         self.loginfo("Walking " + \
-        str(self.params["sequence_length"]["value"]) + " steps")
+        str(self.params["Walk Sequence Length"]["value"]) + " steps")
         steps = []
         
-        L = self.params["stride_length"]["value"]
-        R = self.params["turn_radius"]["value"]
-        W = self.params["stride_width"]["value"]
+        L = self.params["Forward Stride Length"]["value"]
+        L_lat = self.params["Lateral Stride Length"]["value"]
+        R = self.params["Turn Radius"]["value"]
+        W = self.params["Stride Width"]["value"]
         X = 0
         Y = 0
         theta = 0
@@ -137,47 +141,54 @@ class AtlasTeleop():
         
         if forward != 0:
             dTheta = turn * 2 * math.asin(L / (2 * (R + \
-            self.params["stride_width"]["value"]/2)))
+            self.params["Stride Width"]["value"]/2)))
         else:
-            dTheta = turn * self.params["in_place_turn_size"]["value"]
+            dTheta = turn * self.params["In Place Turn Size"]["value"]
         steps = []
         
         # This home step doesn't currently do anything, but it's a 
         # response to bdi not visiting the first step in a trajectory
         home_step = AtlasBehaviorStepData()
-        home_step.foot_index = 1
+        
+        # If moving right, first dummy step is on the left
+        home_step.foot_index = 1*(lateral < 0)
         home_step.pose.position.y = 0.1
         steps.append(home_step)
         prevX = 0
         prevY = 0
         
         # Builds the sequence of steps needed
-        for i in range(self.params["sequence_length"]["value"]):
-            
+        for i in range(self.params["Walk Sequence Length"]["value"]):
             theta += (turn != 0) * dTheta
-            # left = 1, right = -1
-            foot = 1 - 2 * (i % 2)
             
+            # is_right_foot = 1, when stepping with right
+            is_even = i%2
+            is_odd = 1 - is_even
+            is_right_foot = is_even
+            is_left_foot = is_odd
+
+            # left = 1, right = -1            
+            foot = 1 - 2 * is_right_foot
             
             if turn == 0:
                 X = (forward != 0) * (X + forward * L)
-                Y = (lateral != 0) * (Y + lateral * L) - foot * W / 2
+                Y = (lateral != 0) * (Y + is_odd * lateral * L_lat) + foot * W / 2
             elif forward != 0:
-                self.debuginfo("R: " + str(R) + " R_foot:" + \
-                str(R_foot) + " theta: " + str(theta) +  \
-               " math.sin(theta): " + str(math.sin(theta)) + \
-               " math.cos(theta) + " + str(math.cos(theta)))
-                
                 # Radius from point to foot (if turning)
                 R_foot = R + foot * W/2
                 
                 # turn > 0 for CCW, turn < 0 for CW
                 X = forward * turn * R_foot * math.sin(theta)
                 Y = forward * turn * (R - R_foot*math.cos(theta))
+                
+                self.debuginfo("R: " + str(R) + " R_foot:" + \
+                str(R_foot) + " theta: " + str(theta) +  \
+               " math.sin(theta): " + str(math.sin(theta)) + \
+               " math.cos(theta) + " + str(math.cos(theta)))
             elif turn != 0:
                 X = turn * W/2 * math.sin(theta)
                 Y = turn * W/2 * math.cos(theta)
-            
+             
             Q = quaternion_from_euler(0, 0, theta)
             step = AtlasBehaviorStepData()
             
@@ -185,32 +196,35 @@ class AtlasTeleop():
             step.step_index = i+1
             
             # Alternate between feet, start with left
-            step.foot_index = i % 2
-            step.duration = self.params["stride_duration"]["value"]
+            step.foot_index = is_right_foot
+            
+            #If moving laterally to the left, start with the right foot
+            if (lateral > 0):
+                step.foot_index = is_left_foot
+            
+            step.duration = self.params["Stride Duration"]["value"]
             
             step.pose.position.x = X
             step.pose.position.y = Y
-            step.pose.position.z = self.params["step_height"]["value"]
+            step.pose.position.z = self.params["Step Height"]["value"]
          
             step.pose.orientation.x = Q[0]
             step.pose.orientation.y = Q[1]
             step.pose.orientation.z = Q[2]
             step.pose.orientation.w = Q[3]
             
-            step.swing_height = self.params["swing_height"]["value"]
-
-            self.debuginfo("foot: " + str(step.foot_index) + " [" + \
-              str(step.pose.position.x) + ", " + \
-              str(step.pose.position.y) + ", " + str(theta) + "]")            
+            step.swing_height = self.params["Swing Height"]["value"]         
             steps.append(step)
         
         # Add final step to bring feet together
+        self.loginfo("prev step  " + str(steps[len(steps)-1].foot_index))       
+        is_right_foot = 1 - steps[-1].foot_index
+        is_even = is_right_foot
         # foot = 1 for left, foot = -1 for right
-        foot = 1 - 2 * (1 - steps[-1].foot_index)
-        
+        foot = 1 - 2 * is_right_foot
         
         if turn == 0:
-            Y = (lateral != 0) * (Y + lateral * L/2) - foot * W / 2
+            Y = Y - foot * W
         elif forward != 0:
             self.debuginfo("R: " + str(R) + " R_foot:" + \
             str(R_foot) + " theta: " + str(theta) +  \
@@ -229,19 +243,18 @@ class AtlasTeleop():
         Q = quaternion_from_euler(0, 0, theta)
         step = AtlasBehaviorStepData()
         step.step_index = len(steps)
-        step.foot_index = 1 - steps[-1].foot_index
-        step.duration = self.params["stride_duration"]["value"]
+        step.foot_index = is_right_foot
+        step.duration = self.params["Stride Duration"]["value"]
         step.pose.position.x = X
         step.pose.position.y = Y
-        step.pose.position.z = self.params["step_height"]["value"]
+        step.pose.position.z = self.params["Step Height"]["value"]
         step.pose.orientation.x = Q[0]
         step.pose.orientation.y = Q[1]
         step.pose.orientation.z = Q[2]
         step.pose.orientation.w = Q[3]
-        step.swing_height = self.params["swing_height"]["value"]
-        self.debuginfo("foot: " + str(step.foot_index) + \
-          " [" + str(step.pose.position.x) + \
-          ", " + str(step.pose.position.y) + ", " + str(theta) + "]")               
+        step.swing_height = self.params["Swing Height"]["value"]
+        
+        self.loginfo("[foot: " + str(is_right_foot) + ", " + str(X) + ", " + str(Y) + "]")            
         steps.append(step)
 
         # 0 for full BDI control, 255 for PID control
@@ -253,6 +266,10 @@ class AtlasTeleop():
           AtlasBehaviorManipulateParams(),  k_effort )
         
         self.client.send_goal(walk_goal)
+        for step in steps:
+            self.loginfo("foot: " + str(step.foot_index) + \
+              " [" + str(step.pose.position.x) + \
+              ", " + str(step.pose.position.y) + ", " + str(theta) + "]")   
 
     # Select binding values and call twist
     def process_movement(self, ch):
@@ -339,11 +356,11 @@ class AtlasTeleop():
         elif ch == 'q' or ch == 'Q':
             rospy.signal_shutdown("Shutdown")
         try:
-            if (int(ch) >= self.params["sequence_length"]["min"] and \
-                int(ch) <= self.params["sequence_length"]["max"]):
-                self.params["sequence_length"]["value"] = int(ch)
-                self.loginfo("sequence_length: " + \
-                  str(self.params["sequence_length"]["value"]))
+            if (int(ch) >= self.params["Walk Sequence Length"]["min"] and \
+                int(ch) <= self.params["Walk Sequence Length"]["max"]):
+                self.params["Walk Sequence Length"]["value"] = int(ch)
+                self.loginfo("Walk Sequence Length: " + \
+                  str(self.params["Walk Sequence Length"]["value"]))
         except ValueError:
             pass
     
