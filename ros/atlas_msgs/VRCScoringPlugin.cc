17f25d07d209f39ed8677f4a256fe584cd496e24
Brian Gerkey <gerkey@osrfoundation.org>
2013-05-20 17:21:58 -0700

    check that atlas is not in the vehicle when crossing a pedestrian gate, issue #278; still needs testing

--- ros/atlas_msgs/VRCScoringPlugin.cc
+++ ros/atlas_msgs/VRCScoringPlugin.cc
@@ -344,10 +344,13 @@ bool VRCScoringPlugin::CheckNextGate(std::string &_msg)
     switch (this->nextGate->type)
     {
       case Gate::PEDESTRIAN:
+        // We require that Atlas is NOT in the vehicle when it crosses this gate
+        if (this->CheckAtlasInVehicle(tmpString))
+          return false;
         pose = this->atlas->GetWorldPose();
         break;
       case Gate::VEHICLE:
-        // We require that Atlas is in the vehicle when it crosses a gate
+        // We require that Atlas is in the vehicle when it crosses this gate
         if (!this->CheckAtlasInVehicle(tmpString))
           return false;
         pose = this->vehicle->GetWorldPose();
@@ -393,6 +396,11 @@ bool VRCScoringPlugin::CheckNextGate(std::string &_msg)
 /////////////////////////////////////////////////
 bool VRCScoringPlugin::CheckAtlasInVehicle(std::string &_msg)
 {
+  // If we don't know anything about the vehicle (e.g., if this world doesn't
+  // contain a vehicle), then just say no.
+  if (!this->vehicleSeat)
+    return false;
+
   // Where is Atlas?
   math::Vector3 robotPosition = this->atlas->GetWorldPose().pos;
   // Construct bounding box above the seat, using the footprint of the "seat"
