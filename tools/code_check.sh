73ad65788f36c59de4a5fd940f790566dc0afd8a
Jose Luis Rivero <jrivero@osrfoundation.org>
2013-10-17 01:34:35 +0200

    Fix code check with new paths

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+SCRIPT_PATH=$(readlink -f $0 | sed -e 's:/tools/code_check.sh$::g' )
+
 # Jenkins will pass -xml, in which case we want to generate XML output
 xmlout=0
 if test "$1" = "-xmldir" -a -n "$2"; then
@@ -16,14 +18,14 @@ fi
 
 #cppcheck
 if [ $xmlout -eq 1 ]; then
-  (cppcheck --enable=all -q --xml `find ./plugins ./ros -name "*.cc"` --check-config) 2> $xmldir/cppcheck.xml
+  (cppcheck --enable=all -q --xml `find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -name "*.cc"` --check-config) 2> $xmldir/cppcheck.xml
 else
-  cppcheck --enable=all -q `find ./plugins ./ros -name "*.cc"` --check-config
+  cppcheck --enable=all -q `find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -name "*.cc"` --check-config
 fi
 
 # cpplint
 if [ $xmlout -eq 1 ]; then
-  (find ./plugins ./ros -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1) | python tools/cpplint_to_cppcheckxml.py 2> $xmldir/cpplint.xml
+  (find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1) | python tools/cpplint_to_cppcheckxml.py 2> $xmldir/cpplint.xml
 else
-  find ./plugins ./ros -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1
+  find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1
 fi
