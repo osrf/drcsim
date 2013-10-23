30d41b7f17c0d988972f60f9bf111d1490d18c45
Steve Peters <scpeters@osrfoundation.org>
2013-10-23 15:45:04 -0700

    Refactor code_check script a bit to reduce line length and add checking for cpp files

--- tools/code_check.sh
+++ tools/code_check.sh
@@ -16,16 +16,19 @@ else
   builddir=./build
 fi
 
+CHECK_DIRS="$SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins"
+CPPCHECK_FILES=`find $CHECK_DIRS -name "*.cc" -o -name "*.cpp"`
+CPP_LINT_FILES="CPPCHECK_FILES "`find $CHECK_DIRS -name "*.c" -o -name "*.h" -o -name "*.hh"`
 #cppcheck
 if [ $xmlout -eq 1 ]; then
-  (cppcheck --enable=all -q --xml `find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -name "*.cc"` --check-config) 2> $xmldir/cppcheck.xml
+  (cppcheck --enable=all -q --xml $CPPCHECK_FILES --check-config) 2> $xmldir/cppcheck.xml
 else
-  cppcheck --enable=all -q `find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -name "*.cc"` --check-config
+  cppcheck --enable=all -q $CPPCHECK_FILES --check-config
 fi
 
 # cpplint
 if [ $xmlout -eq 1 ]; then
-  (find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1) | python tools/cpplint_to_cppcheckxml.py 2> $xmldir/cpplint.xml
+  (echo $CPP_LINT_FILES | xargs python tools/cpplint.py 2>&1) | python tools/cpplint_to_cppcheckxml.py 2> $xmldir/cpplint.xml
 else
-  find $SCRIPT_PATH/drcsim_gazebo_plugins $SCRIPT_PATH/drcsim_gazebo_ros_plugins -print0 -name "*.cc" -o -name "*.hh" -o -name "*.c" -o -name "*.h" | xargs -0 python tools/cpplint.py 2>&1
+  echo $CPP_LINT_FILES | xargs python tools/cpplint.py 2>&1
 fi
