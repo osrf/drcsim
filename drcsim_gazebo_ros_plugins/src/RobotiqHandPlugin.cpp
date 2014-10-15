bf2f2fccdc09bf1f92a919acb31678817de42f14
Carlos Agüero <caguero@osrfoundation.org>
2014-10-15 11:43:04 -0700

    Style.

--- drcsim_gazebo_ros_plugins/src/RobotiqHandPlugin.cpp
+++ drcsim_gazebo_ros_plugins/src/RobotiqHandPlugin.cpp
@@ -132,7 +132,7 @@ void RobotiqHandPlugin::Load(gazebo::physics::ModelPtr _parent,
   if (!ros::isInitialized())
   {
     gzerr << "Not loading plugin since ROS hasn't been "
-          << "properly initialized. Try starting gazebo with ros plugin:\n"
+          << "properly initialized. Try starting gazebo with ROS plugin:\n"
           << " gazebo -s libgazebo_ros_api_plugin.so\n";
     return;
   }
